1b0e6667f7cc65349d2fd9f15952f1c1659aeb2f
ChelsGreg <cgreg2217@gmail.com>
2019-01-13 13:07:07 -0600

    Changes

---
+++ prob1.js
@@ -0,0 +1,12 @@
+
+
+//Defines a string variable called name and assign it your name
+//
+// Defines a numeric variable called age that contains your age
+//
+// Write the code to log My name is YOURNAME and I am YOURAGE in the developer console
+
+var myName = "Chelsea";
+var age = 29;
+
+console.log("My name is " +myName+ " and I am " +age);
